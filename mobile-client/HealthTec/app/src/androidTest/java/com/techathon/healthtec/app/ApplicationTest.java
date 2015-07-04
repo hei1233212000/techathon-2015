f9226c7fc15f9a9420237a6d3eedfa6731122871
Harry Chan <harry.chan@mensa.org.hk>
2015-07-04 10:44:30 +0800

    created mobile-client project

---
+++ mobile-client/HealthTec/app/src/androidTest/java/com/techathon/healthtec/app/ApplicationTest.java
@@ -0,0 +1,20 @@
+/*
+ * (c) 2015 by Harry Chan
+ * All rights reserved. No part of this document may be reproduced or
+ * transmitted in any form or by any means, electronic, mechanical,
+ * photocopying, recording, or otherwise, without prior written
+ * permission of Harry Chan.
+ */
+package com.techathon.healthtec.app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
