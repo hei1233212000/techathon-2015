2c463b03a9116256f2e2764adead065c306ca417
Paptimus <dennisdominator@gmail.com>
2015-07-04 19:17:30 +0800

    update restful activity

--- mobile-client/HealthTec/app/src/main/java/com/techathon/healthtec/app/MainActivity.java
+++ mobile-client/HealthTec/app/src/main/java/com/techathon/healthtec/app/MainActivity.java
@@ -20,6 +20,8 @@ import android.widget.EditText;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 
+import com.techathon.healthtec.util.RestfulGetActivity;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -27,6 +29,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+
 import java.util.ArrayList;
 
 import java.io.IOException;
@@ -67,8 +70,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
 
-       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-         //       android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //       android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
         // Assign adapter to ListView
         listView.setAdapter(adapter);
@@ -102,7 +105,15 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
 
         b.setClickable(false);
-        new RestfulActivity().execute();
+        new RestfulGetActivity() {
+            @Override
+            public void onPostExecute(String results) {
+                if (results != null) {
+                    EditText et = (EditText) findViewById(R.id.my_edit);
+                    et.setText(results);
+                }
+            }
+        }.execute("https://api-us.clusterpoint.com/100390/Sportiform", "ken.poon@dotcus.com", "12345");
     }
 
     @Override
@@ -125,83 +136,5 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private class RestfulActivity extends AsyncTask<Void, Void, String> {
-        private String url;
-        private String username;
-        private String password;
-
-        public String getUrl() {
-            return this.url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public String getUsername() {
-            return this.username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return this.password;
-        }
-
-        public void setPassowrd(String password) {
-            this.password = password;
-        }
-
-
-        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
-            InputStream in = entity.getContent();
-
-            StringBuffer out = new StringBuffer();
-            int n = 1;
-            while (n > 0) {
-                byte[] b = new byte[4096];
-                n = in.read(b);
-                if (n > 0) out.append(new String(b, 0, n));
-            }
-            return out.toString();
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            url = "https://api-us.clusterpoint.com/100390/Sportiform";
-            username = "ken.poon@dotcus.com";
-            password = "12345";
-
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpContext localContext = new BasicHttpContext();
-            HttpGet httpGet = new HttpGet(url);
-            String text = null;
-            try {
-                String credentials = username + ":" + password;
-                String base64EncodedCredentials = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
-                httpGet.addHeader("Authorization", "Basic " + base64EncodedCredentials);
-                HttpResponse response = httpClient.execute(httpGet, localContext);
-                HttpEntity entity = response.getEntity();
-                text = getASCIIContentFromEntity(entity);
-            } catch (Exception e) {
-                return e.getLocalizedMessage();
-            }
-
-
-            return text;
-        }
-
-
-        protected void onPostExecute(String results) {
-            if (results != null) {
-                EditText et = (EditText) findViewById(R.id.my_edit);
-                et.setText(results);
-            }
-        }
-
     }
 }
